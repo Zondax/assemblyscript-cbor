ad91c4733d536e155bbea8482d02a4f1d349b9d4
Emmanuel <emmanuelm41@gmail.com>
2022-06-09 16:30:50 -0300

    Improvements encoding feature (#2)
    * fix interpreter on IDE
    * fix string and uint8Array methods

--- assembly/cbor.ts
+++ assembly/cbor.ts
@@ -17,49 +17,44 @@ export class CBOREncoder {
     }
 
     serialize(): ArrayBuffer{
-        const ret = new ArrayBuffer(this.offset);
-        const retView = new DataView(ret);
-        for (let i = u32(0); i < this.offset; ++i)
-            retView.setUint8(i, this.dataView.getUint8(i));
-
-        return ret;
+        return this.data.slice(0, this.offset);
     }
 
-    encodeBoolean(value:boolean):void {
+    addBoolean(value:boolean):void {
         if(!value) return this.writeUint8(0xf4);
         this.writeUint8(0xf5);
     }
 
-    encodeNull(value:null):void {
+    addNull(value:null):void {
         this.writeUint8(0xf6);
     }
 
-    encodeUndefined(value:undefined):void {
+    addUndefined(value:undefined):void {
         this.writeUint8(0xf7);
     }
 
-    encodeString(value:string):void {
-        var utf8data: Array<i32> = [];
+    addString(value:string):void {
+        const utf8data: Array<u8> = [];
         for (let i = 0; i < value.length; ++i) {
-            var charCode = value.charCodeAt(i);
+            let charCode = value.charCodeAt(i);
             if (charCode < 0x80) {
-                utf8data.push(charCode);
+                utf8data.push(u8(charCode));
             } else if (charCode < 0x800) {
-                utf8data.push(0xc0 | charCode >> 6);
-                utf8data.push(0x80 | charCode & 0x3f);
+                utf8data.push(u8(0xc0 | charCode >> 6));
+                utf8data.push(u8(0x80 | charCode & 0x3f));
             } else if (charCode < 0xd800) {
-                utf8data.push(0xe0 | charCode >> 12);
-                utf8data.push(0x80 | (charCode >> 6)  & 0x3f);
-                utf8data.push(0x80 | charCode & 0x3f);
+                utf8data.push(u8(0xe0 | charCode >> 12));
+                utf8data.push(u8(0x80 | (charCode >> 6)  & 0x3f));
+                utf8data.push(u8(0x80 | charCode & 0x3f));
             } else {
                 charCode = (charCode & 0x3ff) << 10;
                 charCode |= value.charCodeAt(++i) & 0x3ff;
                 charCode += 0x10000;
 
-                utf8data.push(0xf0 | charCode >> 18);
-                utf8data.push(0x80 | (charCode >> 12)  & 0x3f);
-                utf8data.push(0x80 | (charCode >> 6)  & 0x3f);
-                utf8data.push(0x80 | charCode & 0x3f);
+                utf8data.push(u8(0xf0 | charCode >> 18));
+                utf8data.push(u8(0x80 | (charCode >> 12)  & 0x3f));
+                utf8data.push(u8(0x80 | (charCode >> 6)  & 0x3f));
+                utf8data.push(u8(0x80 | charCode & 0x3f));
             }
         }
 
@@ -67,34 +62,59 @@ export class CBOREncoder {
         this.writeUint8Array(utf8data);
     }
 
-    writeUint8(value:u8):void {
-        this.prepareWrite(1).setUint8(this.offset, value)
+    addArrayU8(value:Array<u8>):void{
+        this.writeTypeAndLength(4, value.length);
+        for (let i = 0; i < value.length; ++i)
+            this.writeTypeAndLength(0, u64(value[i]));
+    }
+
+    addUint8(value:u8):void {
+        this.writeUint8(value)
+    }
+
+    addUint16(value:u16):void {
+        this.writeUint16(value)
+    }
+
+    addUint32(value:u32):void {
+        this.writeUint32(value)
+    }
+
+    addUint64(value:u64):void {
+        this.writeUint64(value)
+    }
+
+    private writeUint8(value:u8):void {
+        this.prepareWrite(1)
+        this.dataView.setUint8(this.offset, value)
         this.commitWrite();
     }
 
-    writeUint16(value:u16):void {
-        this.prepareWrite(2).setUint16(this.offset, value)
+    private writeUint16(value:u16):void {
+        this.prepareWrite(2)
+        this.dataView.setUint16(this.offset, value)
         this.commitWrite();
     }
 
-    writeUint32(value:u32):void {
-        this.prepareWrite(4).setUint32(this.offset, value)
+    private writeUint32(value:u32):void {
+        this.prepareWrite(4)
+        this.dataView.setUint32(this.offset, value)
         this.commitWrite();
     }
 
-    writeUint64(value:u64):void {
-        var low = value % POW_2_32;
-        var high = (value - low) / POW_2_32;
-        var dataView = this.prepareWrite(8);
-        dataView.setUint32(this.offset, high);
-        dataView.setUint32(this.offset + 4, low);
+    private writeUint64(value:u64):void {
+        const low = u32(value % POW_2_32);
+        const high = u32((value - low) / POW_2_32);
+        this.prepareWrite(8);
+        this.dataView.setUint32(this.offset, high);
+        this.dataView.setUint32(this.offset + 4, low);
         this.commitWrite();
     }
 
-    writeUint8Array(value:Array<i32>):void {
-        const dataView = this.prepareWrite(value.length);
-        for (var i = 0; i < value.length; ++i)
-            dataView.setUint8(this.offset + i, value[i]);
+    private writeUint8Array(value:Array<u8>):void {
+        this.prepareWrite(value.length);
+        for (let i = 0; i < value.length; ++i)
+            this.dataView.setUint8(this.offset + i, value[i]);
         this.commitWrite();
     }
 
@@ -102,7 +122,7 @@ export class CBOREncoder {
         this.offset += this.lastLength;
     }
 
-    private prepareWrite(length:u32):DataView {
+    private prepareWrite(length:u32):void {
         let newByteLength = u32(this.data.byteLength);
         const requiredLength = this.offset + length;
         while (newByteLength < requiredLength)
@@ -117,25 +137,24 @@ export class CBOREncoder {
         }
 
         this.lastLength = length;
-        return this.dataView;
     }
 
 
     private writeTypeAndLength(type:u8, length:u64):void {
         if (length < 24) {
-            this.writeUint8(type << 5 | length);
+            this.writeUint8(u8(type << 5 | length));
         } else if (length < 0x100) {
-            this.writeUint8(type << 5 | 24);
-            this.writeUint8(length);
+            this.writeUint8(u8(type << 5 | 24));
+            this.writeUint8(u8(length));
         } else if (length < 0x10000) {
-            this.writeUint8(type << 5 | 25);
-            this.writeUint16(length);
+            this.writeUint8(u8(type << 5 | 25));
+            this.writeUint16(u16(length));
         } else if (length < 0x100000000) {
-            this.writeUint8(type << 5 | 26);
-            this.writeUint32(length);
+            this.writeUint8(u8(type << 5 | 26));
+            this.writeUint32(u32(length));
         } else {
-            this.writeUint8(type << 5 | 27);
-            this.writeUint64(length);
+            this.writeUint8(u8(type << 5 | 27));
+            this.writeUint64(u64(length));
         }
     }
 
